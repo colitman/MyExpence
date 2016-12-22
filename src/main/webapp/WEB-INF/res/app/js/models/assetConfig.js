2b7bf55a0254d8d43914a556623173d1f1f3f9b3
Dmytro Romenskyi <d.romenskyi@gmail.com>
2016-12-22 19:57:44 +0200

    [ci skip] assetConfig model refactored;

--- src/main/webapp/WEB-INF/res/app/js/models/assetConfig.js
+++ src/main/webapp/WEB-INF/res/app/js/models/assetConfig.js
@@ -1,25 +1,61 @@
 "use strict";
 
-/*
-Model is an observable object.
-It should expose the following public access interfaces:
-- subscribe
-
-It also should have the following private methods:
-- countObservers
-- isChanged
-- setChanged
-- clearChanged
-- notifyObservers
-- deleteObservers
- */
-
 (function(aScope, undefined){
 	
 	var observable = new Observable();
 	
 	var assetModel = {
 		__proto__: observable,
+		
+		updateData: function() {
+			var _this = this;
+			
+			var vm = {
+				asset:{},
+				currencies: {
+					total:0,
+					listData:[]
+				},
+				paymentSystems: {
+					total:0,
+					listData:[]
+				}
+			};
+			
+			_this.getAsset()
+				.done(function(assetData) {
+					vm.asset = assetData;
+					
+					_this.getPaymentSystems()
+						.done(function(paymentSystemsData) {
+							vm.paymentSystems.total = paymentSystemsData.length;
+							vm.paymentSystems.listData = paymentSystemsData;
+							
+							aScope.currencyService.getCurrencies()
+								.done(function(currenciesData) {
+									vm.currencies.total = currenciesData.length;
+									vm.currencies.listData = currenciesData;
+									
+									aScope.VM = vm;
+									
+									_this.setChanged();
+									_this.notifyObservers(aScope.VM, 'asset:dataUpdated');
+								})
+								.fail(function(jqXHR, textStatus, errorThrown) {
+									new Alert('danger', 'Oops!', 'Failed to get currencies.').show();
+									console.log(jqXHR.responseText);
+								});
+						})
+						.fail(function(jqXHR, textStatus, errorThrown) {
+							new Alert('danger', 'Oops!', 'Failed to get payment systems.').show();
+							console.log(jqXHR.responseText);
+						});
+				})
+				.fail(function(jqXHR, textStatus, errorThrown) {
+					new Alert('danger', 'Oops!', 'Failed to get asset.').show();
+					console.log(jqXHR.responseText);
+				});
+		},
 		
 		getPaymentSystems: function() {
 			return aScope.paymentSystemService.getPaymentSystems();
@@ -34,8 +70,8 @@ It also should have the following private methods:
 			var _this = this;
 			return aScope.assetService.updateAsset(assetData)
 				.done(function(data) {
-					_this.setChanged();
-					_this.notifyObservers(_this, 'updated');
+					_this.updateData();
+					new Alert('success', 'Success!', 'Asset has been successfully updated.').show();
 				});
 		}
 	
