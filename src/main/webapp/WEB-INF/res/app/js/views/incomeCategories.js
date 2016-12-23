cbec3ac43f2e70e000c678ce5984a3562e1a9981
Dmytro Romenskyi <d.romenskyi@gmail.com>
2016-12-23 15:02:10 +0200

    [ci skip] income categories view refactored

--- src/main/webapp/WEB-INF/res/app/js/views/incomeCategories.js
+++ src/main/webapp/WEB-INF/res/app/js/views/incomeCategories.js
@@ -1,144 +1,55 @@
 "use strict";
 
-/*
-View is an observer object.
-It should expose the following public access interfaces:
-- update(object)
- 
- View is also an observable object.
- It should expose the following public access interfaces:
- - subscribe
- 
- It also should have the following private methods:
- - countObservers
- - isChanged
- - setChanged
- - clearChanged
- - notifyObservers
- - deleteObservers
-*/
-
 (function(aScope, undefined){
 	
+	var observer = new Observer();
+	
+	var incomeCategoriesView = {
+		
+		__proto__: observer,
+		
+		/**
+		 * This method is called by observed model when it is changed.
+		 *
+		 * @param {*} subject - reference to VM object in global application scope with model data
+		 * @param {string} [message=undefined] - additional message that model may send
+		 */
+		update: function(subject, message) {
+			resetForms();
+			hideModals();
+			buildIncomeCategoriesList(subject);
+			initDataTable();
+		}
+		
+	};
+	
+	aScope.incomeCategoriesView = incomeCategoriesView;
+	
+	/* Private fields */
 	var addCategoryForm = $('#c-js-add-income-category-form');
 	var editCategoryForm = $('#c-js-edit-income-category-form');
 	var categoriesTable = $('#c-js-income-categories-table');
 	var deleteModal = $('#c-delete-confirmation-modal');
 	var deleteIncomeCategoryForm = $('#c-modal-delete-form');
 	
-	var categoryAddedEvent = new ViewEvent('c.category.added',{form: addCategoryForm, type: $EX.CATEGORY_TYPES.INCOME});
-	var categoryChangeEvent = new ViewEvent('c.category.change');
-	var categoryChangedEvent = new ViewEvent('c.category.changed',editCategoryForm);
-	var categoryDeleteEvent = new ViewEvent('c.category.delete');
-	var categoryDeletedEvent = new ViewEvent('c.category.deleted');
-	
-	var updateIncomeCategoriesList = function(data) {
-		var oldTbody = $('tbody', categoriesTable);
-		var body = document.createElement('tbody');
-		
-		for(var i = 0; i < data.length; i++) {
-			var row = document.createElement('tr');
-			
-			var tdName = document.createElement('td');
-			
-			$(tdName).text(data[i].name);
-			
-			$(row).append(tdName);
-			$(row).append(createIncomeCategoryActions(data[i]));
-			
-			$(body).append(row);
-		}
-		
-		
-		$(oldTbody).remove();
-		$('thead', categoriesTable).after(body)
-	}
-	
-	var createIncomeCategoryActions = function(categoryData) {
-		var tdActions = document.createElement('td');
-		
-		var seeTxAction = document.createElement('a');
-		$(seeTxAction).attr('href', $EX.APP_ROOT + '/settings/categories/' + categoryData.id + '/transactions');
-		$(seeTxAction).html('<i class="fa fa-database"></i>');
-		$(tdActions).append(seeTxAction);
-		
-		var editAction = document.createElement('a');
-		$(editAction).attr('href', '#');
-		$(editAction).html('<i class="fa fa-pencil"></i>');
-		$(editAction).data('target', categoryData.id);
-		$(tdActions).append(editAction);
-		
-		$(editAction).click(function(event) {
-			event.preventDefault();
-			categoryChangeEvent.data = {id: $(editAction).data('target'), form:editCategoryForm};
-			incomeCategoriesView.setChanged();
-			incomeCategoriesView.notifyObservers(categoryChangeEvent);
-		});
-		
-		var deleteAction = document.createElement('a');
-		$(deleteAction).attr('href', '#');
-		$(deleteAction).html('<i class="fa fa-remove"></i>');
-		$(deleteAction).data('target', categoryData.id);
-		$(tdActions).append(deleteAction);
-		
-		$(deleteAction).click(function(event) {
-			event.preventDefault();
-			categoryDeleteEvent.data = $(deleteAction).data('target');
-			incomeCategoriesView.setChanged();
-			incomeCategoriesView.notifyObservers(categoryDeleteEvent);
-		});
-		
-		return tdActions;
-	}
-	
-	var initDataTable = function() {
-		
-		var txDataTable = $('.c-js-datatable').DataTable({
-			dom: 'rt<<"col-sm-6"li><"col-sm-6 text-right"p>>',
-			order: [[0,'asc']],
-			pagingType: 'full_numbers',
-			lengthMenu: [[10,25,50,-1],[10,25,50,'All']],
-			destroy: true
-		});
-	}
-	
-	var observable = new Observable();
-	
-	var incomeCategoriesView = {
-		
-		__proto__: observable,
-		
-		update: function(categoriesModel) {
-			categoriesModel.getIncomeCategories()
-				.done(function(categoriesData) {
-					$('button[type="reset"]', addCategoryForm).click();
-					$(deleteModal).modal('hide');
-					$('[type="reset"]', editCategoryForm).click();
-					$(editCategoryForm).addClass('hidden');
-					updateIncomeCategoriesList(categoriesData);
-					initDataTable();
-				})
-				.fail(function(jqXHR) {
-					console.log(jqXHR.responseText);
-				});
-		}
-	
-	};
-	
+	/* View events triggers */
 	addCategoryForm.submit(function(event) {
 		event.preventDefault();
-		incomeCategoriesView.setChanged();
-		incomeCategoriesView.notifyObservers(categoryAddedEvent)
+		$(incomeCategoriesView).trigger('category:added', [$EX.CATEGORY_TYPES.INCOME, addCategoryForm]);
 	});
 	
 	editCategoryForm.submit(function(event) {
 		event.preventDefault();
-		incomeCategoriesView.setChanged();
-		incomeCategoriesView.notifyObservers(categoryChangedEvent)
+		$(incomeCategoriesView).trigger('category:changed', [editCategoryForm]);
+	});
+	
+	deleteIncomeCategoryForm.submit(function(event) {
+		event.preventDefault();
+		$(incomeCategoriesView).trigger('category:deleted', [$('#id', deleteIncomeCategoryForm), deleteModal])
 	});
 	
 	$('[type="reset"]', editCategoryForm).click(function(event) {
-		$('[type="hidden"]', editCategoryForm).each(function(undex, hiddenInput) {
+		$('[type="hidden"]', editCategoryForm).each(function(index, hiddenInput) {
 			$(hiddenInput).val('');
 		});
 		editCategoryForm.addClass('hidden');
@@ -146,18 +57,96 @@ It should expose the following public access interfaces:
 	
 	$('#c-js-cancel-edit-incoming-category-button', editCategoryForm).click(function(event) {
 		event.preventDefault();
-		categoryChangeEvent.data = {id: $('#id', editCategoryForm).val(), form:editCategoryForm};
-		incomeCategoriesView.setChanged();
-		incomeCategoriesView.notifyObservers(categoryChangeEvent);
+		$(incomeCategoriesView).trigger('category:change', [$('#id', editCategoryForm).val(), editCategoryForm]);
 	});
 	
-	deleteIncomeCategoryForm.submit(function(event) {
-		event.preventDefault();
-		categoryDeletedEvent.data = $(deleteIncomeCategoryForm);
-		incomeCategoriesView.setChanged();
-		incomeCategoriesView.notifyObservers(categoryDeletedEvent);
-	});
+	/* Private methods */
+	var resetForms = function() {
+		$('button[type="reset"]', addCategoryForm).click();
+		$('[type="reset"]', editCategoryForm).click();
+		$(editCategoryForm).addClass('hidden');
+	};
+	
+	var hideModals = function() {
+		$(deleteModal).modal('hide');
+	};
+	
+	var initDataTable = function() {
 		
-	aScope.incomeCategoriesView = incomeCategoriesView;
+		var txDataTable = $('.c-js-datatable').DataTable({
+			dom: 'rt<<"col-sm-6"li><"col-sm-6 text-right"p>>',
+			order: [[0,'asc']],
+			pagingType: 'full_numbers',
+			lengthMenu: [[10,25,50,-1],[10,25,50,'All']]
+		});
+	};
+	
+	var buildIncomeCategoriesList = function(vm) {
+		var categories = vm.income.listData;
+		
+		$('#c-js-income-categories .c-js-datatable').each(function(index, table) {
+			if ( $.fn.dataTable.isDataTable( table ) ) {
+				$(table).DataTable().destroy();
+			}
+		});
+		
+		var body = $('tbody', categoriesTable);
+		body.html('');
+		
+		for(var i = 0; i < categories.length; i++) {
+			var category = categories[i];
+			
+			var row = jQueryDomBuilder(
+				[
+					category.name,
+					''
+				],
+				body
+			);
+			
+			var actionsColumn = row.find('td:last');
+			var seeTxAction = jQueryDomBuilder.getAnchor(
+				$EX.APP_ROOT + '/settings/categories/' + category.id + '/transactions',
+				'',
+				[
+					['target',category.id]
+				],
+				actionsColumn
+			);
+			$(seeTxAction).html('<i class="fa fa-database"></i>');
+			
+			var editAction = jQueryDomBuilder.getAnchor(
+				'#',
+				'',
+				[
+					['target',category.id]
+				],
+				actionsColumn
+			);
+			editAction.html('<i class="fa fa-pencil"></i>');
+			
+			var deleteAction = jQueryDomBuilder.getAnchor(
+				'#',
+				'',
+				[
+					['target',category.id]
+				],
+				actionsColumn
+			);
+			deleteAction.html('<i class="fa fa-remove"></i>');
+			
+			$(editAction).click(function(event) {
+				event.preventDefault();
+				$(incomeCategoriesView).trigger('category:change', [$(this).data('target'), editCategoryForm]);
+			});
+			
+			
+			
+			$(deleteAction).click(function(event) {
+				event.preventDefault();
+				$(incomeCategoriesView).trigger('category:delete', [$(this).data('target'), deleteModal]);
+			});
+		}
+	};
 	
 })($EX);
